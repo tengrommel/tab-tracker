bc7482215080b4c66b43eaf6a9ded7c6e05bafe4
teng <wwwsousoucom@126.com>
2017-10-01 17:17:24 +0800

    vue

---
+++ server/src/models/index.js
@@ -0,0 +1,28 @@
+const fs = require('fs')
+const path = require('path')
+const Sequelize = require('sequelize')
+const config = require('../config/config')
+const db = {}
+
+const sequelize = new Sequelize(
+  config.db.database,
+  config.db.user,
+  config.db.password,
+  config.db.options
+)
+
+// 链式调用 将models文件架下除去index.js文件所有的model导入到db
+fs
+  .readdirSync(__dirname)
+  .filter((file) =>
+    file !== 'index.js'
+  )
+  .forEach((file) => {
+    const model = sequelize.import(path.join(__dirname, file))
+    db[model.name] = model
+  })
+
+db.sequelize = sequelize
+db.Sequelize = Sequelize
+
+module.exports = db
